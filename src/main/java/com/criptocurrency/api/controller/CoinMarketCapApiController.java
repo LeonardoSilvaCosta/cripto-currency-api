e1721edffa03e870f17b53579645ae868a0a5562
leonardocosta <sir.costa@yahoo.com.br>
2022-08-14 13:08:56 -0300

    Alteração de nome do método makeApiCall to listCriptoCurrencies e url de listagem de criptomoedas inserida dentro do referido método

--- src/main/java/com/criptocurrency/api/controller/CoinMarketCapApiController.java
+++ src/main/java/com/criptocurrency/api/controller/CoinMarketCapApiController.java
@@ -1,7 +1,10 @@
 package com.criptocurrency.api.controller;
 
 import com.criptocurrency.api.model.CoinMarketApiData;
-import org.springframework.http.*;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -12,13 +15,15 @@ import org.springframework.web.client.RestTemplate;
 public class CoinMarketCapApiController {
 
     private String apiKey = "c4f98fc4-8cf3-413c-bfce-75a6bb5ed3e7";
-    private String url = "https://pro-api.coinmarketcap.com/v1/cryptocurrency/listings/latest";
 
     @GetMapping
-    public ResponseEntity<CoinMarketApiData> makeAPiCall() {
+    public ResponseEntity<CoinMarketApiData> listCriptoCurrencies() {
+        String url = "https://pro-api.coinmarketcap.com/v1/cryptocurrency/listings/latest";
+
         RestTemplate restTemplate = new RestTemplate();
         HttpHeaders headers = new HttpHeaders();
         headers.set("X-CMC_PRO_API_KEY", apiKey);
+        headers.set("Accept", "application/json");
         HttpEntity requestEntity = new HttpEntity(headers);
         ResponseEntity<CoinMarketApiData> response = restTemplate.exchange(url, HttpMethod.GET, requestEntity, CoinMarketApiData.class);
 
